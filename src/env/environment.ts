e3c8572c8d4b3ef20ee778fcf6fdb9c27aa635be
Renato Corbellini <renacorbe@gmail.com>
2024-01-14 17:49:29 -0300

    Update environment.ts

--- src/env/environment.ts
+++ src/env/environment.ts
@@ -4,4 +4,4 @@ export const environment = {
 
 // export const baseUrl = 'http://localhost:8080/api';
 export const baseUrl =
-  'https://odontogram-api-5688f29f9751.herokuapp.com';
+  'https://odontogram-api-5688f29f9751.herokuapp.com/api';
